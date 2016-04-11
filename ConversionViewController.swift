140b66e71a6af5d5865564e9cea44b96701dbee2
adrpadua <adrpadua@gmail.com>
2016-04-11 14:19:19 -0700

    Converts temperatures now

---
+++ ConversionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  File.swift
+//  WorldTrotter
+//
+//  Created by Adrian Padua on 4/11/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+}
