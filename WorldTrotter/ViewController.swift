037e524b2381255edc043410afb36ad84bc5d48d
adrpadua <adrpadua@gmail.com>
2016-04-11 13:30:51 -0700

    Begin Project

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,12 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150) // create frame for view
+        let firstView = UIView(frame: firstFrame) // create view to go inside frame
+        firstView.backgroundColor = UIColor.blueColor() // set view color to blue inside frame
+        view.addSubview(firstView) // add the view to its frame
     }
 
 
