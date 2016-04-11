6891c8e18ca54eaf9c2e8b05d38a114e782a4c1f
adrpadua <adrpadua@gmail.com>
2016-04-11 13:56:24 -0700

    Views and Layout placed

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -10,15 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150) // create frame for view
-        let firstView = UIView(frame: firstFrame) // create view to go inside frame
-        firstView.backgroundColor = UIColor.blueColor() // set view color to blue inside frame
-        view.addSubview(firstView) // add the view to its frame
-    }
-
-
+    
 }
 
